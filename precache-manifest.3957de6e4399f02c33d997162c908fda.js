9941894ac8efd2302c3d49043b37519f40aa8cb9
c3rb1n <ilya_prudnikov97@mail.ru>
2020-11-12 15:17:53 +0300

    Updates

---
+++ precache-manifest.3957de6e4399f02c33d997162c908fda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f1932cd4676939402519242552b61d1",
+    "url": "/ToDo-React/index.html"
+  },
+  {
+    "revision": "bb416951ee6918551696",
+    "url": "/ToDo-React/static/css/main.c7812c65.chunk.css"
+  },
+  {
+    "revision": "090d595b4b99ecf2ad84",
+    "url": "/ToDo-React/static/js/2.36186b26.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ToDo-React/static/js/2.36186b26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb416951ee6918551696",
+    "url": "/ToDo-React/static/js/main.4673c0b6.chunk.js"
+  },
+  {
+    "revision": "ded17d0a6a120a9f8875",
+    "url": "/ToDo-React/static/js/runtime-main.d94dc21d.js"
+  }
+]);
